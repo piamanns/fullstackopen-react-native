96b411c0afd434f5a68be111487390c4bf101ac2
Pia Manns <pia.manns@gmail.com>
2024-09-23 12:57:02 +0300

    Add exercise 10.7 (make app bar scrollable horizontally)

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 import theme from '../theme';
@@ -6,8 +6,6 @@ import theme from '../theme';
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    display: 'flex',
-    flexDirection: 'row',
     backgroundColor: theme.colors.bgAppBar,
   }
 });
@@ -15,8 +13,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab text="Repositories" route="/"/>
-      <AppBarTab text="Sign in" route="/signin"/>
+      <ScrollView horizontal>
+        <AppBarTab text="Repositories" route="/"/>
+        <AppBarTab text="Sign in" route="/signin"/>
+      </ScrollView>
     </View>
   );
 };
